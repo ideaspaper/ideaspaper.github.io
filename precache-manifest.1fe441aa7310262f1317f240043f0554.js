ab4d0e6fe4ffa1a788d1f748c7f1352b87bd7ea8
ideaspaper <agni.infinity@gmail.com>
2020-05-03 20:04:40 +0700

    Updates

---
+++ precache-manifest.1fe441aa7310262f1317f240043f0554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7211cacdd1f6107c81aa28a647b01e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db3f7b91000fa399297a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad59c9013a229e62fd99",
+    "url": "/static/js/2.1639a2bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1639a2bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3f7b91000fa399297a",
+    "url": "/static/js/main.1aac6fbd.chunk.js"
+  },
+  {
+    "revision": "144aa7728a4a609cab0b",
+    "url": "/static/js/runtime-main.947e0e51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
