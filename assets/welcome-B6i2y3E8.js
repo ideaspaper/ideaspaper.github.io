df6009abbaf9a551f10b9fade7e9012498084115
anpan <agni.infinity@gmail.com>
2026-01-07 14:44:39 +0700

    Updates

---
+++ assets/welcome-B6i2y3E8.js
@@ -0,0 +1,25 @@
+const n=`---
+title: Welcome to My Blog
+date: 2024-05-20
+description: This is the very first post on my new React-based markdown blog.
+---
+
+# Hello World!
+
+Welcome to my simple, **markdown-powered** blog.
+
+## Features
+
+- Written in React
+- Uses Markdown for content
+- Dark & Light mode supported
+- Simple and clean design
+
+Here is a code snippet:
+
+\`\`\`javascript
+console.log('Hello from the blog!');
+\`\`\`
+
+Enjoy reading!
+`;export{n as default};
